4a22eca671816acc729083b29f2b9832c76eb472
IOhannes m zmölnig <zmoelnig@iem.at>
2013-09-11 17:06:40 +0200

    recursive path-creation
    http://stackoverflow.com/questions/675039/how-can-i-create-directory-tree-in-c-linux
    (c) 2009 Jonathan Leffler
    
    "You are hereby given permission to use this code for any purpose with attribution."

--- ladspa_utils.c
+++ ladspa_utils.c
@@ -14,10 +14,67 @@
 #include <math.h>
 
 #include <sys/stat.h>
-#include <sys/types.h>
 
 #include <ladspa.h>
 #include "ladspa_utils.h"
+
+
+/* the following do_mkdir() and mkpath() implementation is (c) 2009 Jonathan Leffler
+ * under follwing license:
+ *  You are hereby given permission to use this code for any purpose with attribution.
+ */
+typedef struct stat Stat;
+static int do_mkdir(const char *path, mode_t mode)
+{
+    Stat            st;
+    int             status = 0;
+
+    if (stat(path, &st) != 0)
+    {
+        /* Directory does not exist. EEXIST for race condition */
+        if (mkdir(path, mode) != 0 && errno != EEXIST)
+            status = -1;
+    }
+    else if (!S_ISDIR(st.st_mode))
+    {
+        errno = ENOTDIR;
+        status = -1;
+    }
+
+    return(status);
+}
+
+/**
+** mkpath - ensure all directories in path exist
+** Algorithm takes the pessimistic view and works top-down to ensure
+** each directory in path exists, rather than optimistically creating
+** the last element and working backwards.
+*/
+int mkpath(const char *path, mode_t mode)
+{
+    char           *pp;
+    char           *sp;
+    int             status;
+    char           *copypath = strdup(path);
+
+    status = 0;
+    pp = copypath;
+    while (status == 0 && (sp = strchr(pp, '/')) != 0)
+    {
+        if (sp != pp)
+        {
+            /* Neither root nor double slash in path */
+            *sp = '\0';
+            status = do_mkdir(copypath, mode);
+            *sp = '/';
+        }
+        pp = sp + 1;
+    }
+    if (status == 0)
+        status = do_mkdir(path, mode);
+    free(copypath);
+    return (status);
+}
 
 /* ------------------------------------------------------------------ */
 
@@ -296,7 +353,7 @@ LADSPA_Control * LADSPAcontrolMMAP(const LADSPA_Descriptor *psDescriptor,
 			return NULL;
 		}
 		sprintf(filename, "%s%s", homePath, subdir);
-    if(mkdir(filename, 0770)) {
+    if(mkpath(filename, 0770)) {
       return NULL;
     }
 
