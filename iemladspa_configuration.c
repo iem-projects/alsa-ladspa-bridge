7d7e7d0b7b62ff67d36e258b499029c095f70d16
IOhannes m zmölnig <zmoelnig@iem.at>
2013-11-04 16:10:13 +0100

    asoundrc config-parser

---
+++ iemladspa_configuration.c
@@ -0,0 +1,231 @@
+/*
+ * alsa-ladspa-bridge: use LADSPA-plugins as ALSA-plugins
+ *
+ * Copyright (c) 2013 IOhannes m zmölnig - IEM
+ *		<zmoelnig@iem.at>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "iemladspa_configuration.h"
+
+#include <string.h>
+
+void iemladspa_config_print(iemladspa_config_t*conf) {
+  fprintf(stderr, "conf: %p\n", conf);
+  fprintf(stderr, "\ttype=%d\n", conf->type);
+  fprintf(stderr, "\tcontrolfile=%s\n", conf->controlfile);
+  fprintf(stderr, "\tsourcechannels=%d/%d\n",
+          conf->channels[SND_PCM_STREAM_CAPTURE].in,
+          conf->channels[SND_PCM_STREAM_CAPTURE].out);
+  fprintf(stderr, "\tsinkchannels=%d/%d\n",
+          conf->channels[SND_PCM_STREAM_PLAYBACK].in,
+          conf->channels[SND_PCM_STREAM_PLAYBACK].out);
+  fprintf(stderr, "\tformat=%d\n", conf->format);
+  fprintf(stderr, "\tlibrary=%s\n", conf->ladspa_library);
+  fprintf(stderr, "\tmodule =%s\n", conf->ladspa_module);
+}
+
+iemladspa_config_t*iemladspa_config_create(iemladspa_config_type_t type) {
+  iemladspa_config_t*conf=(iemladspa_config_t*)malloc(sizeof(iemladspa_config_t));
+
+  if(conf) {
+    /* fill with default values */
+    conf->controlfile=NULL;
+
+    conf->channels[SND_PCM_STREAM_CAPTURE].in   = 2;
+    conf->channels[SND_PCM_STREAM_CAPTURE].out  = 2;
+    conf->channels[SND_PCM_STREAM_PLAYBACK].in  = 2;
+    conf->channels[SND_PCM_STREAM_PLAYBACK].out = 2;
+
+    conf->format  = SND_PCM_FORMAT_S16;
+
+    conf->ladspa_library=strdup("/usr/lib/ladspa/iemladspa.so");
+    conf->ladspa_module =strdup("iemladspa");
+
+    conf->type = type;
+  }
+  return conf;
+}
+
+static int iemladspa_subconfig_parse (iemladspa_config_t*CONF, snd_config_t*conf, const char*name, int dir) {
+  snd_config_iterator_t i, next;
+  snd_config_for_each(i, next, conf) {
+    snd_config_t *n = snd_config_iterator_entry(i);
+    const char *id;
+    long channels=0;
+    if (snd_config_get_id(n, &id) < 0)
+      continue;
+
+    if (strcmp(id, "inchannels") == 0) {
+      snd_config_get_integer(n, &channels);
+      if(channels < 1) {
+        SNDERR("%s.inchannels %d < 1", name, channels);
+        return -EINVAL;
+      }
+      CONF->channels[dir].in =channels;
+      continue;
+    }
+
+    if (strcmp(id, "outchannels") == 0) {
+      snd_config_get_integer(n, &channels);
+      if(channels < 1) {
+        SNDERR("%s.outchannels %d < 1", name, channels);
+        return -EINVAL;
+      }
+      CONF->channels[dir].out =channels;
+      continue;
+    }
+    SNDERR("unknown config %s.%s", name, id);
+    return -EINVAL;
+  }
+  return 0;
+
+}
+
+
+void iemladspa_config_free(iemladspa_config_t*conf) {
+  if(conf->controlfile)free((void*)conf->controlfile);
+  conf->controlfile=NULL;
+
+    conf->channels[SND_PCM_STREAM_CAPTURE].in   = 0;
+    conf->channels[SND_PCM_STREAM_CAPTURE].out  = 0;
+    conf->channels[SND_PCM_STREAM_PLAYBACK].in  = 0;
+    conf->channels[SND_PCM_STREAM_PLAYBACK].out = 0;
+
+  conf->format = SND_PCM_FORMAT_UNKNOWN;
+
+  if(conf->ladspa_library)free((void*)conf->ladspa_library);
+  conf->ladspa_library=NULL;
+
+  if(conf->ladspa_module)free((void*)conf->ladspa_module);
+  conf->ladspa_module=NULL;
+
+  conf->type = none;
+    
+  free(conf);
+}
+
+int iemladspa_config_init(iemladspa_config_t*CONF, snd_config_t*conf) {
+  snd_config_iterator_t i, next;
+  snd_config_t *sconf = NULL;
+  const char*str=NULL;
+  char *controls = NULL;
+  const char *configname = NULL;
+
+  if (snd_config_get_id(conf, &configname) < 0)
+    configname=NULL;
+
+  /* Parse configuration options from asoundrc */
+  snd_config_for_each(i, next, conf) {
+    snd_config_t *n = snd_config_iterator_entry(i);
+
+    const char *id;
+    if (snd_config_get_id(n, &id) < 0)
+      continue;
+    if (strcmp(id, "comment") == 0 || strcmp(id, "type") == 0 || strcmp(id, "hint") == 0)
+      continue;
+    if (strcmp(id, "slave") == 0) {
+      sconf = n;
+      continue;
+    }
+    if (strcmp(id, "controls") == 0) {
+      snd_config_get_string(n, &str);
+      controls=(char*)str;
+      continue;
+    }
+    if (strcmp(id, "library") == 0) {
+      snd_config_get_string(n, &str);
+      if(CONF->ladspa_library)free((void*)CONF->ladspa_library);
+      CONF->ladspa_library=strdup(str);
+      continue;
+    }
+    if (strcmp(id, "module") == 0) {
+      snd_config_get_string(n, &str);
+      if(CONF->ladspa_module)free((void*)CONF->ladspa_module);
+      CONF->ladspa_module=strdup(str);
+      continue;
+    }
+    if (strcmp(id, "format") == 0) {
+      int format;
+      snd_config_get_string(n, &str);
+      format=snd_pcm_format_value(str);
+      if(SND_PCM_FORMAT_S16!=format && SND_PCM_FORMAT_FLOAT!=format) {
+        SNDERR("format must be %s or %s", snd_pcm_format_name(SND_PCM_FORMAT_S16), snd_pcm_format_name(SND_PCM_FORMAT_FLOAT));
+        return -EINVAL;
+      }
+      CONF->format = format;
+      continue;
+    }
+    if (strcmp(id, "inchannels") == 0) {
+      long channels = 0;
+      snd_config_get_integer(n, &channels);
+      if(channels < 1) {
+        SNDERR("channels < 1");
+        return -EINVAL;
+      }
+      CONF->channels[SND_PCM_STREAM_CAPTURE].in =channels;
+      CONF->channels[SND_PCM_STREAM_CAPTURE].out=channels;
+      continue;
+    }
+    if (strcmp(id, "outchannels") == 0) {
+      long channels = 0;
+      snd_config_get_integer(n, &channels);
+      if(channels < 1) {
+        SNDERR("outchannels < 1");
+        return -EINVAL;
+      }
+      CONF->channels[SND_PCM_STREAM_PLAYBACK].in =channels;
+      CONF->channels[SND_PCM_STREAM_PLAYBACK].out=channels;
+      continue;
+    }
+    if (strcmp(id, "source") == 0) {
+      int er = iemladspa_subconfig_parse(CONF, n, id, SND_PCM_STREAM_CAPTURE);
+      if(er)return er;
+      continue;
+    }
+    if (strcmp(id, "sink") == 0) {
+      int er = iemladspa_subconfig_parse(CONF, n, id, SND_PCM_STREAM_PLAYBACK);
+      if(er)return er;
+      continue;
+    }
+    SNDERR("Unknown field %s", id);
+    return -EINVAL;
+  }
+
+  /* Make sure we have a slave and control devices defined */
+  if (! sconf) {
+    SNDERR("No slave configuration for iemladspa pcm");
+    return -EINVAL;
+  }
+
+  if(controls) {
+    if(CONF->controlfile)free((void*)CONF->controlfile);
+    CONF->controlfile=strdup(controls);
+  } else if (configname) {
+    controls=(char*)calloc(strlen(configname)+5, 1);
+    if(!controls) {
+      SNDERR("unable to allocate memory for '%s.bin'", configname);
+      return -EINVAL;
+    }
+    sprintf(controls, "%s.bin", configname);
+    if(CONF->controlfile)free((void*)CONF->controlfile);
+    CONF->controlfile=controls;
+  }
+
+  iemladspa_config_print(CONF);
+  return 0;
+}
+
+
---
+++ iemladspa_configuration.h
@@ -0,0 +1,49 @@
+/*
+ * alsa-ladspa-bridge: use LADSPA-plugins as ALSA-plugins
+ *
+ * Copyright (c) 2013 IOhannes m zmölnig - IEM
+ *		<zmoelnig@iem.at>
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IEMLADSPA_CONFIGURATION_H
+#define IEMLADSPA_CONFIGURATION_H
+
+#include "ladspa_utils.h"
+#include <alsa/asoundlib.h>
+
+typedef enum {
+  none = -1,
+  PCM,
+  CTL
+} iemladspa_config_type_t;
+
+typedef struct iemladspa_config_ {
+  iemladspa_config_type_t type;
+
+  const char*controlfile;
+  iemladspa_iochannels_t channels[SND_PCM_STREAM_LAST+1];
+  unsigned int format;
+
+  /* LADSPA */
+  const char*ladspa_library;
+  const char*ladspa_module;
+} iemladspa_config_t;
+
+iemladspa_config_t*iemladspa_config_create(iemladspa_config_type_t type);
+int iemladspa_config_init(iemladspa_config_t*conf, snd_config_t *alsaconf);
+void iemladspa_config_free(iemladspa_config_t*conf);
+
+#endif /* IEMLADSPA_CONFIGURATION_H */
